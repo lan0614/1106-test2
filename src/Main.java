eb6e76d7993bbd08ec03b3d1390619641ce57212
lan0614 <sniper86614@gmail.com>
2017-11-06 15:07:22 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //星座判定
+        int m, d;
+        m = in.nextInt();
+        d = in.nextInt();
+        switch (m) {
+            case 1:
+                if (d < 21) {
+                    System.out.println("Capricorn\n");
+                }
+                else
+                    System.out.println("Aquarius\n");
+                    break;
+
+                }
+        }
+    }
